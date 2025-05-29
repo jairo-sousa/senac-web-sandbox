a665cd330dcd8fdde5c07daed22a5ab16a9c492c
Jairo Sousa <jairo.sousa.rufino@gmail.com>
2025-05-29 23:47:41 +0000

    Answer question 10

---
+++ script/question_10.js
@@ -0,0 +1,29 @@
+// Exercicio 10 - Crie um programa que verifique se um número é múltiplo de outro número e exiba o resultado no console.
+
+window.addEventListener("load", () => {
+  const evaluateBtn = document.getElementById("evaluateBtn");
+  const referenceNumber = document.getElementById("referenceNumber");
+  const numberToCompare = document.getElementById("numberToCompare");
+  const numberInfo = document.getElementById("numberInfo");
+
+  evaluateBtn.onclick = (event) => {
+    event.preventDefault();
+
+    response = evaluateNumbers(
+      Number(referenceNumber.value),
+      Number(numberToCompare.value)
+    );
+
+    console.log(response);
+
+    numberInfo.innerText = response;
+  };
+});
+
+const evaluateNumbers = (reference, compare) => {
+  let result = "";
+
+  if (compare % reference === 0) result = "é ";
+
+  return `O número comparado ${result}múltiplo`;
+};
