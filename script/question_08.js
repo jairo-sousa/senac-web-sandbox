3b4aa93a76995cdc07e3da2197d9dee70f12cfb3
Jairo Sousa <jairo.sousa.rufino@gmail.com>
2025-05-29 23:07:15 +0000

    Answer question 08

---
+++ script/question_08.js
@@ -0,0 +1,26 @@
+// Questão 08 - Crie um programa que verifique se um número é positivo, negativo ou zero e exiba o resultado no console.
+
+window.addEventListener("load", () => {
+  const evaluateBtn = document.getElementById("evaluateBtn");
+  const numberToEval = document.getElementById("numberToEval");
+  const numberInfo = document.getElementById("numberInfo");
+
+  evaluateBtn.onclick = (event) => {
+    event.preventDefault();
+
+    response = evaluateNumber(Number(numberToEval.value));
+
+    console.log(response);
+
+    numberInfo.innerText = response;
+  };
+});
+
+const evaluateNumber = (number) => {
+  let result = "";
+  if (number > 0) result = "Positivo";
+  if (number < 0) result = "Negativo";
+  if (number === 0) result = "Zero";
+
+  return `O número é ${result}`;
+};
---
+++ script/question_09.js
@@ -0,0 +1,27 @@
+// Questão 09 - Crie um programa que verifique se um número é maior, menor ou igual a outro número e exiba o resultado no console.
+
+window.addEventListener("load", () => {
+  const evaluateBtn = document.getElementById("evaluateBtn");
+  const numberToEval = document.getElementById("numberToEval");
+  const numberInfo = document.getElementById("numberInfo");
+
+  evaluateBtn.onclick = (event) => {
+    event.preventDefault();
+
+    response = evaluateNumber(Number(numberToEval.value));
+
+    console.log(response);
+
+    numberInfo.innerText = response;
+  };
+});
+
+const evaluateNumber = (number) => {
+  let result = "";
+  console.log(number, typeof number);
+  if (number > 0) result = "Positivo";
+  if (number < 0) result = "Negativo";
+  if (number === 0) result = "Zero";
+
+  return `O número é ${result}`;
+};
